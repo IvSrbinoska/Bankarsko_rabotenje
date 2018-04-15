2d252ae9640213b2081fbc6b6294e0e1bf3b8128
mk2010 <vladislav.skoro@gmail.com>
2018-04-15 10:07:14 -0400

    test sql statements za zadaca 5

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,6 +1,31 @@
-﻿CREATE view dbo.PregledKarticki
-as
-SELECT ci.CardNumber, ci.print_name, ci.print_surname, ci.[status],ci.embg_user,name_series
-FROM cards_issued ci
-left join card_products cp
-On ci.IDProduct = cp.IDProduct
+﻿--CREATE PROCEDURE dbo.sp_Obrabotka
+--    @datum DATETIME
+--AS
+--SELECT * 
+--FROM Person.Address
+--WHERE City = @City
+--GO
+
+-- tip na transakcija=-1 e provizija!?
+DECLARE @datum DATE
+SET @datum='2018-04-23'
+
+SELECT MONTH(@datum)
+
+SELECT a.*,b.*
+FROM dbo.card_products a INNER JOIN dbo.cards_issued b ON a.IDProduct=b.IDProduct
+INNER JOIN dbo.card_transactions c ON b.CardNumber=c.CardNumber AND c.tip_transakcija=-1
+WHERE a.debit_credit=1 AND b.[status]=1 AND MONTH(c.date_processing)=MONTH(@datum)
+
+
+
+
+/*
+SELECT * FROM dbo.card_transactions
+WHERE tip_transakcija=-1
+
+SELECT * FROM dbo.VRABOTEN1
+SELECT * FROM dbo.ADRESAR
+SELECT * FROM dbo.cards_issued
+SELECT * FROM dbo.card_products
+*/
